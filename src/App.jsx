339e7ad6708fdee28e25ab2746ed480dc7cd2826
mogubess <51812354+mogubess@users.noreply.github.com>
2022-07-18 20:20:53 +0900

    add useEffect add useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
-import React, { useState } from "react";
-// import React from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+  const onClickSwitchShowFalg = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  //空の配列[]を渡すと最初の1回だけ呼ばれる
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちわ!</h1>
       <ColorfulMessage color="blue">お元気ですか-?</ColorfulMessage>
       <ColorfulMessage color="pink">元気です!</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFalg}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>(#^.^#)</p>}
     </>
   );
 };
